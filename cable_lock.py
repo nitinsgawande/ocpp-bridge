9740312f85b81b87631edef3d407322d4a82ef18
nitin.s.gawande <nitin.gawande@bt.com>
2026-06-04 21:20:18 +0530

    updated local host url

--- cable_lock.py
+++ cable_lock.py
@@ -23,9 +23,7 @@ import redis
 log = logging.getLogger(__name__)
 
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
-    db=0,
+    os.getenv("REDIS_URL", "redis://localhost:6379/0"),
     decode_responses=True
 )
 
